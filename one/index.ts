dc6bf5f070215e1434aa69739cc9b2fd8a348857
Shovgur <s.showgur@yandex.by>
2024-01-23 16:11:38 +0300

    fix: test

--- one/index.ts
+++ one/index.ts
@@ -1,21 +1,33 @@
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 
-const input = fs.readFileSync(path.join(__dirname, "./index.txt")).toString();
+// const input = fs.readFileSync(path.join(__dirname, "./index.txt")).toString();
 
-const lines = input.split("\n").filter((line) => line.trim() !== "");
+// const lines = input.split("\n").filter((line) => line.trim() !== "");
 
-const result = lines
-  .map((line) => {
-    const numberArray = line //'ab3' < string
-      .split("") // ['a', 'b', '3']... и т.д < string[]
-      .map(Number) // [ NaN, NaN, 3, ] ... и т.д < number[]
-      .filter((number) => !Number.isNaN(number)); // [3]; < number[]
-    const first = numberArray[0];
-    const last = numberArray[numberArray.length - 1];
+// const result = lines
+//   .map((line) => {
+//     const numberArray = line //'ab3' < string
+//       .split("") // ['a', 'b', '3']... и т.д < string[]
+//       .map(Number) // [ NaN, NaN, 3, ] ... и т.д < number[]
+//       .filter((number) => !Number.isNaN(number)); // [3]; < number[]
+//     const first = numberArray[0];
+//     const last = numberArray[numberArray.length - 1];
 
-    return first * 10 + last;
-  })
-  .reduce((acc, item) => acc + item, 0);
+//     return first * 10 + last;
+//   })
+//   .reduce((acc, item) => acc + item, 0);
 
-console.log(result);
+// console.log(result);
+ 
+
+const a = {}
+
+function clear(obj) {
+    obj.x = 40 
+    obj = null
+}
+
+clear(a)
+
+console.log(a)    
